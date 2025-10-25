7d800b1c9c19dcc90bd09874aeb23abf0cda3879
buihaiduongdev <haiduongtinhoc@gmail.com>
2025-10-25 04:33:23 +0000

    add shop filter

--- src/main/java/com/example/demo/dto/ProductSearchCriteria.java
+++ src/main/java/com/example/demo/dto/ProductSearchCriteria.java
@@ -1,42 +1,51 @@
 package com.example.demo.dto;
 
 public class ProductSearchCriteria {
-	private String keyword;
-	private String categorySlug;
-	private String brandSlug;
-	private String sort;
+    private String keyword;
+    private String categorySlug;
+    private String brandSlug;
+    private String sort;
+    private Integer shopId; // Add shopId field
 
-	// Getters
-	public String getKeyword() {
-		return keyword;
-	}
+    // Getters
+    public String getKeyword() {
+        return keyword;
+    }
 
-	public String getCategorySlug() {
-		return categorySlug;
-	}
+    public String getCategorySlug() {
+        return categorySlug;
+    }
 
-	public String getBrandSlug() {
-		return brandSlug;
-	}
+    public String getBrandSlug() {
+        return brandSlug;
+    }
 
-	public String getSort() {
-		return sort;
-	}
+    public String getSort() {
+        return sort;
+    }
 
-	// Setters
-	public void setKeyword(String keyword) {
-		this.keyword = keyword;
-	}
+    public Integer getShopId() {
+        return shopId;
+    }
 
-	public void setCategorySlug(String categorySlug) {
-		this.categorySlug = categorySlug;
-	}
+    // Setters
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
 
-	public void setBrandSlug(String brandSlug) {
-		this.brandSlug = brandSlug;
-	}
+    public void setCategorySlug(String categorySlug) {
+        this.categorySlug = categorySlug;
+    }
 
-	public void setSort(String sort) {
-		this.sort = sort;
-	}
+    public void setBrandSlug(String brandSlug) {
+        this.brandSlug = brandSlug;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public void setShopId(Integer shopId) {
+        this.shopId = shopId;
+    }
 }
