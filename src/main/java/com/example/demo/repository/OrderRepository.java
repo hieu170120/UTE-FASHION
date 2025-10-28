da4f5de9935e5e68d7038d226558bbd7c68a4d2d
DuyPhan422 <phanngocduypnd@gmail.com>
2025-10-28 21:19:45 +0700

    fix anh chi tiet don 4 role

--- src/main/java/com/example/demo/repository/OrderRepository.java
+++ src/main/java/com/example/demo/repository/OrderRepository.java
@@ -50,6 +50,7 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     @Query("SELECT DISTINCT o FROM Order o " +
            "LEFT JOIN FETCH o.orderItems oi " +
+           "LEFT JOIN FETCH oi.product " +
            "LEFT JOIN FETCH o.user " +
            "LEFT JOIN FETCH o.shipper " +
            "LEFT JOIN FETCH o.carrier " +
